75521fefac00dbe1f371abf4efd2176eea99fb97
Luiz Felippe Gonçalves <felippedev@hotmail.com>
2017-12-05 00:15:51 -0200

    Observables study in progress

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -1,7 +1,12 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit, OnDestroy } from '@angular/core'
 import { ActivatedRoute } from '@angular/router'
 import { OfertasService } from '../ofertas.service'
 import { Oferta } from '../shared/oferta.model'
+import { Observable } from 'rxjs/Observable'
+import { Observer } from 'rxjs/Observer'
+import { Subscription } from 'rxjs/Subscription';
+
+import 'rxjs/Rx'
 
 @Component({
   selector: 'app-oferta',
@@ -9,8 +14,11 @@ import { Oferta } from '../shared/oferta.model'
   styleUrls: ['./oferta.component.css'],
   providers: [OfertasService]
 })
-export class OfertaComponent implements OnInit {
+export class OfertaComponent implements OnInit, OnDestroy {
 
+  private tempoObservableSubscription: Subscription
+  private meuObservableSubscription: Subscription
+  
   public oferta: Oferta
 
   constructor(
@@ -18,12 +26,6 @@ export class OfertaComponent implements OnInit {
     private ofertasService: OfertasService) { }
 
   ngOnInit() {
-    console.log('[Snapshot] Parametro da rota: ' + this.route.snapshot.params['id'])
-
-    // this.route.params.subscribe((parametro: any) => {
-    //   console.log('[Subscribe] Parametro da rota: ' + parametro.id)
-    // })
-
     this.ofertasService.getOfertasPorId(this.route.snapshot.params['id'])
       .then((response: Oferta) => {
         this.oferta = response
@@ -32,6 +34,35 @@ export class OfertaComponent implements OnInit {
         console.log('Erro: Não foi possível buscar a oferta por id')
         console.log(reason)
       })
+
+    // this.route.params.subscribe((parametro: any) => {
+    //   console.log('[Subscribe] Parametro da rota: ' + parametro.id)
+    // })
+
+    let tempo = Observable.interval(500)
+
+    this.tempoObservableSubscription = tempo.subscribe((intervalo: number) => {
+      console.log(intervalo);
+    })
+
+    //observable
+    let meuObservableTeste = Observable.create((observer: Observer<string>) => {
+      observer.next('Evento 1')
+      observer.next('Evento 2')
+      // observer.error('Error!')
+      observer.complete()
+    })
+
+    //observer
+    this.meuObservableSubscription = meuObservableTeste.subscribe(
+      (resultado: string) => console.log(resultado),
+      (erro: string) => console.log(erro),
+      () => console.log('Completado!')
+    )
   }
 
+  ngOnDestroy() {
+    this.tempoObservableSubscription.unsubscribe()
+    this.meuObservableSubscription.unsubscribe()
+  }
 }
